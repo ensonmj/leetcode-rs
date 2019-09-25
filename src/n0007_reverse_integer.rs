8fa2a603840cec3d2d590dd7fca87de96cc2291e
ensonmj <ensonmj@gmail.com>
2019-09-25 20:12:43 +0800

    7: fix guard_rem

--- src/n0007_reverse_integer.rs
+++ src/n0007_reverse_integer.rs
@@ -1,34 +1,34 @@
-    pub fn reverse(x: i32) -> i32 {
-        if x == i32::min_value() {
+pub fn reverse(x: i32) -> i32 {
+    if x == i32::min_value() {
+        return 0;
+    }
+
+    let mut res: i32 = 0;
+
+    let mut y = x.abs();
+    let guard = i32::max_value() / 10;
+    let mut guard_rem = i32::max_value() % 10;
+    if x < 0 {
+        guard_rem += 1;
+    }
+    while y != 0 {
+        if res > guard {
             return 0;
         }
-
-        let mut res: i32 = 0;
-
-        let mut y = x.abs();
-        let guard = i32::max_value() / 10;
-        let mut guard_rem = i32::max_value() % 10;
-        if x < 0 {
-            guard_rem -= 1;
+        let rem = y % 10;
+        if res == guard && rem > guard_rem {
+            return 0;
         }
-        while y != 0 {
-            if res > guard {
-                return 0;
-            }
-            let rem = y % 10;
-            if res == guard && rem > guard_rem {
-                return 0;
-            }
-            res = res * 10 + rem;
-            y = y / 10;
-        }
-
-        if x > 0 {
-            res
-        } else {
-            -res
-        }
+        res = res * 10 + rem;
+        y = y / 10;
     }
+
+    if x > 0 {
+        res
+    } else {
+        -res
+    }
+}
 
 #[cfg(test)]
 mod tests {
